fbdb4cc6b48ba40f589e5b8946a1e1c1c7c4fe40
Stephanie Nemeth <traumverloren@gmail.com>
2016-04-09 16:52:43 +0200

    finish styling for Home component, add input form

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,13 +1,26 @@
 var React = require('react');
 var homeBg = require('../styles').homeBg;
-
+var yellowBg = require('../styles').yellowBg;
+var flexContainer = require('../styles').flexContainer;
 var Home = React.createClass({
   render: function () {
     return (
       <div className="container-fluid" style={homeBg}>
-        <div className="jumbotron">
-          <h2>Hello World!</h2>
-          Home.js Container!
+        <div className="jumbotron col-xs-8 text-center" style={yellowBg}>
+          <h1 style={{color: '#f1c40f'}}>Enter a City</h1>
+          <div style={flexContainer}>
+            <form>
+              <div className="form-group">
+                <input className="form-control"
+                       placeholder="City, State"
+                       type="text"
+                       style={{fontFamily: 'Quicksand'}} />
+              </div>
+              <div className="form-group">
+                <button className="btn btn-success" style={{fontFamily: 'Quicksand'}}>Get Weather</button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     )
--- app/styles/index.js
+++ app/styles/index.js
@@ -13,6 +13,19 @@ var styles = {
     height: '100%',
     width: '100%',
     minHeight: '100%'
+  },
+
+  yellowBg: {
+    background: 'rgba(25, 25, 25, .3)',
+    fontFamily: 'Sacramento'
+  },
+
+  flexContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center'
   }
 };
 
