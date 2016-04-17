57d7fc5b22c5ed0236c4f26efc6d85b8d2a7e082
Stephanie Nemeth <traumverloren@gmail.com>
2016-04-17 18:28:49 +0200

    update props when getWeather button is submitted while on forecast page

--- app/containers/ForecastContainer.js
+++ app/containers/ForecastContainer.js
@@ -7,14 +7,23 @@ var ForecastContainer = React.createClass({
 // will fetch forecast api data
 // once componentdidmount isLoading: false, forecastData
   getInitialState: function() {
-    console.log(this.props);
     return {
       isLoading: true,
       forecastData: {}
     };
   },
   componentDidMount: function() {
-    openWeatherHelpers.getForecast(this.props.params.city)
+    console.log(this.props);
+    openWeatherHelpers.getForecast(this.props.routeParams.city)
+      .then(function (forecastData) {
+          this.setState({
+            forecastData: forecastData,
+            isLoading: false
+          })
+        }.bind(this));
+  },
+  componentWillReceiveProps: function(nextProps) {
+    openWeatherHelpers.getForecast(nextProps.routeParams.city)
       .then(function (forecastData) {
           this.setState({
             forecastData: forecastData,
